56e076641232baa6dce367fb1179cce37461285d
unaidelao <unai.delao@gmail.com>
2019-05-19 01:46:27 +0200

    modified main.rs

--- first-steps/src/main.rs
+++ first-steps/src/main.rs
@@ -1,5 +1,32 @@
-mod print;
+// mod print;
+// mod variables;
+// mod data_types;
+// mod strings;
+// mod tuples;
+// mod tuples;
+// mod arrays;
+// mod vectors;
+// mod conditionals;
+// mod loops;
+// mod functions;
+// mod pointers_and_references;
+// mod structs;
+// mod enums;
+mod cli;
 
 fn main() {
-    print::run(); 
+    // print::run();
+    // variables::run();
+    // data_types::run();
+    // strings::run();
+    // tuples::run();
+    // arrays::run();
+    // vectors::run();
+    // conditionals::run();
+    // loops::run();
+    // functions::run();
+    // pointers_and_references::run();
+    // structs::run();
+    // enums::run();
+    cli::run();
 }
